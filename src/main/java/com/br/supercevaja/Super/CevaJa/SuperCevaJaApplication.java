0f179864f20feb8c1984fffc8d4636d71e6217c0
rlangbecker <rlangbecker94@gmail.com>
2023-06-13 19:26:16 -0300

    Estrutura inicial

---
+++ src/main/java/com/br/supercevaja/Super/CevaJa/SuperCevaJaApplication.java
@@ -0,0 +1,13 @@
+package com.br.supercevaja.Super.CevaJa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCevaJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperCevaJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/supercevaja/Super/CevaJa/SuperCevaJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.supercevaja.Super.CevaJa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCevaJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
